0ce7f1c2582614a88444ce55fc94e93a62ee8f73
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2023-04-19 11:26:13 +0300

    test

--- lib/draggable_home.dart
+++ lib/draggable_home.dart
@@ -261,7 +261,7 @@ class _DraggableHomeState extends State<DraggableHome> {
                     ),
                     child: FlexibleSpaceBar(
                       collapseMode: CollapseMode.none,
-                      stretchModes: const [],
+                      stretchModes: const [StretchMode.zoomBackground],
                       background: Container(
                         margin: const EdgeInsets.only(bottom: 0.2),
                         child: fullyExpanded
